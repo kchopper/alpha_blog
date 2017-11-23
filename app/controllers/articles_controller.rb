4c28d4fa540925955a53c4508f14daafc847c818
kchopper <hamza.bhinder@gmail.com>
2017-11-23 11:05:04 +0500

    reduced repitition in the code and made it ready for production

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
 
   def index
     @article = Article.all    
@@ -19,15 +20,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
+    
     if @article.update(article_params)
       flash[:notice] = "Your blog has been successfully updated"
       redirect_to article_path(@article)
@@ -37,7 +36,6 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] ="Article was deleted"
     redirect_to articles_path
@@ -45,6 +43,9 @@ class ArticlesController < ApplicationController
   end
 
 private
+  def set_article
+    @article = Article.find(params[:id])
+  end
   def article_params
     params.require(:article).permit(:title, :description)
   end
