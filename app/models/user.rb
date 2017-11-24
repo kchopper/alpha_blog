d5d9e34bcd237b495b837f69c378b575acc4105b
kchopper <hamza.bhinder@gmail.com>
2017-11-24 14:39:33 +0500

    User signin complete with form and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
+  has_many :articles
+  before_save { self.email = email.downcase }
+  has_secure_password
   
+  validates :username, presence: true, 
+            length: {minimum: 3, maximum:16}, 
+            uniqueness: {case_sensitive: false}  
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email, presence: true, length:{minimum: 3, maximum: 105}, 
+            uniqueness:{case_sensitive: false},   
+            format: {with: VALID_EMAIL_REGEX}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171123103204) do
+ActiveRecord::Schema.define(version: 20171124081248) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string "email"
